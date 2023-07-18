dd59af20f7e3f59950fd0c6b3c9cfe5fc7cadf92
devLorran <lorranrodrigues553@gmail.com>
2023-07-17 23:47:04 -0300

    Clone responsivo da Netflix feito com reactjs com o consumo da API The movie DB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import './App.css'
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+export default () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(true);
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+
+      //pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      //pegando somente as séries
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+    }
+
+    loadAll()
+  }, [])
+  
+  //adicionando um evento de monitoramento da própria página
+  useEffect(() => {
+    const scrollListenenr = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true)
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListenenr);
+
+    return () => {
+      window.addEventListener('scroll', scrollListenenr);
+    }
+  }, []);
+
+  return(
+    <div className='page'>
+
+      <Header black={blackHeader}/>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+        Feito por LorranCode
+        Direitos de imagem para Netflix<br/>
+        Dados pegos no https://Themoviedb.org
+      </footer>
+
+      {/* LOADING */}
+      {movieList.length <= 0 && 
+      <div className='loading'>
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt='Carregando' />
+      </div>
+      }
     </div>
-  );
+  )
 }
-
-export default App;
